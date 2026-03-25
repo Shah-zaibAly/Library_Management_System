d5ff2ad13d63138ff6085681ba213b06f11ada07
Shah-zaibAly <shahxaib269@gmail.com>
2026-03-25 22:43:25 +0500

    feat : added the pm.xml file

--- src/main/java/com/nust/lms/model/Borrowable.java
+++ src/main/java/com/nust/lms/model/Borrowable.java
@@ -1,10 +1,10 @@
-package com.nust.lms.model;
+
 /**
  * Interface demonstrates ABSTRACTION.
  * It defines behavior ("what" should happen) without implementation details ("how").
  * Any class that can be issued/returned can implement this contract.
  */
 public interface Borrowable {
-    boolean issueTo(Member member);
-    boolean returnFrom(Member member);
+    boolean issueTo(com.nust.lms.model.Member member);
+    boolean returnFrom(com.nust.lms.model.Member member);
 }
