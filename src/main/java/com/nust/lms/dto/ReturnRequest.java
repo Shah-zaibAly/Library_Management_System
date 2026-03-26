ccdf317ceebe50fc588fae6a8d3c3de58402934d
Shah-zaibAly <shahxaib269@gmail.com>
2026-03-26 10:09:03 +0500

    feat : added the ReturnRequest class

---
+++ src/main/java/com/nust/lms/dto/ReturnRequest.java
@@ -0,0 +1,38 @@
+package com.nust.lms.dto;
+
+import jakarta.validation.constraints.Positive;
+
+/**
+ * Request body for POST /return
+ * Frontend will send: { "bookId": 101, "memberId": 1 }
+ */
+public class ReturnRequest {
+    @Positive
+    private int bookId;
+
+    @Positive
+    private int memberId;
+
+    public ReturnRequest() {}
+
+    public ReturnRequest(int bookId, int memberId) {
+        this.bookId = bookId;
+        this.memberId = memberId;
+    }
+
+    public int getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(int bookId) {
+        this.bookId = bookId;
+    }
+
+    public int getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(int memberId) {
+        this.memberId = memberId;
+    }
+}
