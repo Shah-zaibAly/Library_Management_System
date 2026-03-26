ec205f72bb47a0d2cca8927cac52a9af10556ecc
Shah-zaibAly <shahxaib269@gmail.com>
2026-03-26 10:28:00 +0500

    feat : created the LmsApiApplication class in Lms package

---
+++ src/main/java/com/nust/lms/LmsApiApplication.java
@@ -0,0 +1,11 @@
+package com.nust.lms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LmsApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LmsApiApplication.class, args);
+    }
+}
